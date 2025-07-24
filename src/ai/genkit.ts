8f6dd530246abfb41a916f7e7b5fc9bd7c4e594a
Ninad Saswade <ninadsaswade87@gmail.com>
2025-07-24 10:29:50 +0000

    Same error i think you should pin point instead of changing the code may

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,8 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [googleAI({
     // Make sure to set your Google Cloud project ID in your environment variables.
-    // project: process.env.GOOGLE_CLOUD_PROJECT_ID
+    // project: process.env.GOOGLE_CLOUD_PROJECT_ID,
+    // location: 'us-central1'
   })],
-  model: 'googleai/gemini-1.5-flash-latest',
+  model: 'gemini-1.5-flash',
 });
